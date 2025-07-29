93c0894edbd0f3c7d98069a7def62640860ee3d2
PratikshaPM <pratikshamahajan2@gmail.com>
2025-07-29 22:16:00 +0530

    Added meals route page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function Meals() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals created
+          <span className={classes.highlight}> by you</span>
+        </h1>
+        <p>Choose your faviour reciepe and cook yourself</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share you favourite reciepe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
